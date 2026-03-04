284812611ac403a9a7d4b9da4b6689e58432f06f
sofiavalero09 <valerosofia974@gmail.com>
2026-03-04 22:23:11 +0000

    ejecutar

--- Momento1/Ejecutar.java
+++ Momento1/Ejecutar.java
@@ -3,96 +3,76 @@ import java.util.Random;
 
 public class Ejecutar {
 
-    static Buque[] buques = new Buque[10];
-    static Matriz gestion = new Matriz();
-    static Scanner sc = new Scanner(System.in);
-    static Random r = new Random();
-
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Random r = new Random();
+
+        Buque[] buques = new Buque[10];
+        Matriz gestion = new Matriz();
 
         int opcion;
 
         do {
-            System.out.println("MENU");
+
             System.out.println("1. Registrar Buque");
             System.out.println("2. Registrar Contenedor");
-            System.out.println("3. Mostrar Peso");
+            System.out.println("3. Mostrar Peso Total");
             System.out.println("4. Mostrar Matriz");
             System.out.println("5. Salir");
 
             opcion = sc.nextInt();
             sc.nextLine();
 
-            switch (opcion) {
+            if (opcion == 1) {
 
-                case 1:
-                    registrarBuque();
-                    break;
+                for (int i = 0; i < buques.length; i++) {
+                    if (buques[i] == null) {
 
-                case 2:
-                    registrarContenedor();
-                    break;
+                        System.out.print("Nombre del buque: ");
+                        String nombre = sc.nextLine();
 
-                case 3:
-                    System.out.println("Peso total: " + gestion.calcularPesoTotal());
-                    break;
+                        System.out.print("Capacidad: ");
+                        int capacidad = sc.nextInt();
+                        sc.nextLine();
 
-                case 4:
-                    gestion.mostrarMatriz();
-                    break;
+                        buques[i] = new Buque(nombre, capacidad);
+                        System.out.println("Buque registrado.");
+                        break;
+                    }
+                }
 
-                case 5:
-                    System.out.println("fin.");
-                    break;
+            } else if (opcion == 2) {
 
-                default:
-                    System.out.println("invalido.");
+                System.out.print("Columna (0-9): ");
+                int col = sc.nextInt();
+                sc.nextLine();
+
+                System.out.print("Codigo: ");
+                String codigo = sc.nextLine();
+
+                System.out.print("Origen: ");
+                String origen = sc.nextLine();
+
+                int peso = r.nextInt(26) + 10; // 10 a 35
+
+                System.out.println("Peso: " + peso);
+
+                Contenedores nuevo = new Contenedores(codigo, origen, peso);
+                gestion.registrarContenedor(col, nuevo);
+
+            } else if (opcion == 3) {
+
+                System.out.println("Peso total: " + gestion.calcularPesoTotal());
+
+            } else if (opcion == 4) {
+
+                gestion.mostrarMatriz();
+
             }
 
         } while (opcion != 5);
-    }
 
-    public static void registrarBuque() {
-
-        for (int i = 0; i < buques.length; i++) {
-            if (buques[i] == null) {
-
-                System.out.print("Nombre del buque: ");
-                String nombre = sc.nextLine();
-
-                System.out.print("Capacidad: ");
-                int capacidad = sc.nextInt();
-                sc.nextLine();
-
-                buques[i] = new Buque(nombre, capacidad);
-                System.out.println("Buque registrado " + i);
-                return;
-            }
-        }
-
-        System.out.println("No hay más espacio.");
-    }
-
-    public static void registrarContenedor() {
-
-        System.out.print("Columna (0-9): ");
-        int col = sc.nextInt();
-        sc.nextLine();
-
-        System.out.print("Código: ");
-        String codigo = sc.nextLine();
-
-        System.out.print("Origen: ");
-        String origen = sc.nextLine();
-
-      
-        int min = 10;
-        int max = 35;
-        double peso = r.nextInt((max - min + 1)) + min;
-
-        System.out.println("Peso: " + peso);
-
-        Contenedores nuevo = new Contenedores(codigo, origen, peso);
-        gestion.registrarContenedor(col, nuevo);
+        System.out.println("fin.");
     }
 }
