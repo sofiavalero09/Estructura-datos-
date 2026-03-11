2103abbf5d44d87a9389f9051094e51d696f47cc
sofiavalero09 <valerosofia974@gmail.com>
2026-03-11 21:10:24 +0000

    .

--- Parcial/Ejecutar.java
+++ Parcial/Ejecutar.java
@@ -1,38 +1,28 @@
 import java.util.Random;
-public class Ejecutar {
-public static void main(String[] args) {
-    Random r =new Random ();
 
-    int[]energiaContenedores = new int[12];
-    for (int i = 0; i < energiaContenedores.length; i++) {
-        energiaContenedores[i]= r.nextInt(101)+50;
-    }
-    System.out.println("Energia generada:");
-    for (int e : energiaContenedores) {
-            System.out.print(e + " ");
+public class Ejecutar {
+
+    public static void main(String[] args) {
+
+        Random r = new Random();
+
+        int[] energiaContenedores = new int[12];
+
+        for (int i = 0; i < energiaContenedores.length; i++) {
+            energiaContenedores[i] = r.nextInt(101) + 50;
         }
 
         int[] filtrados = new int[12];
         int contador = 0;
 
         for (int i = 0; i < energiaContenedores.length; i++) {
-
-            if (energiaContenedores[i] % 10 == 0) { 
-                
-
+            if (energiaContenedores[i] % 10 == 0) {
                 filtrados[contador] = energiaContenedores[i];
                 contador++;
             }
         }
 
-        System.out.println("Energia filtrada (multiplos de 10):");
-        for (int i = 0; i < contador; i++) {
-            System.out.print(filtrados[i] + " ");
-        }
-
-
-
-        int[][] mapaCarga = new int[3][3]; 
+        int[][] mapaCarga = new int[3][3];
 
         int index = 0;
 
@@ -46,15 +36,6 @@ public static void main(String[] args) {
                     mapaCarga[i][j] = -1;
                 }
             }
-        }
-
-        System.out.println("\nMapa de carga:");
-
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(mapaCarga[i][j] + " ");
-            }
-            System.out.println();
         }
 
         Suministro[] manifiesto = new Suministro[9];
@@ -79,8 +60,8 @@ public static void main(String[] args) {
                     String id = "C-" + i + "-" + j;
 
                     manifiesto[pos] = new Suministro(id, valor, prioridad);
-                } 
-                else {
+
+                } else {
                     manifiesto[pos] = null;
                 }
 
@@ -88,15 +69,12 @@ public static void main(String[] args) {
             }
         }
 
-
-        System.out.println("\nManifiesto de vuelo:");
-
         for (int i = 0; i < manifiesto.length; i++) {
 
             if (manifiesto[i] != null) {
                 System.out.println(manifiesto[i]);
             } else {
-                System.out.println("Posicion vacia");
+                System.out.println("null");
             }
         }
     }
--- Parcial/Suministro.java
+++ Parcial/Suministro.java
@@ -1,20 +1,16 @@
 public class Suministro {
 
-    String id;          
-    int nivelEnergia;   
-    String prioridad;   
+    String id;
+    int nivelEnergia;
+    String prioridad;
 
-    
     public Suministro(String id, int nivelEnergia, String prioridad) {
         this.id = id;
         this.nivelEnergia = nivelEnergia;
         this.prioridad = prioridad;
     }
 
-    
     public String toString() {
         return id + " | Energia: " + nivelEnergia + " | Prioridad: " + prioridad;
     }
 }
-
-
