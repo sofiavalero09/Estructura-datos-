463232277924a41837e9ea097fa7596d8e12d122
sofiavalero09 <valerosofia974@gmail.com>
2026-04-15 22:45:16 +0000

    Buque

---
+++ Momento 2/Contenedor.java
@@ -0,0 +1,29 @@
+public class Contenedor {
+
+    private String id;
+    private double peso;
+    private int prioridad;
+
+    public Contenedor(String id, double peso, int prioridad) {
+        this.id = id;
+        this.peso = peso;
+        this.prioridad = prioridad;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public int getPrioridad() {
+        return prioridad;
+    }
+
+    @Override
+    public String toString() {
+        return "ID: " + id + " | Peso: " + peso + " | Prioridad: " + prioridad;
+    }
+}
