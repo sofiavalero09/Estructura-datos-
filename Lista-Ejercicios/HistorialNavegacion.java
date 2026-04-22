c1a722293c436bf97939b8d54278213e1774449b
sofiavalero09 <valerosofia974@gmail.com>
2026-04-22 20:06:29 +0000

    Ejercicio mapas

--- Lista-Ejercicios/HistorialNavegacion.java
+++ Lista-Ejercicios/HistorialNavegacion.java
@@ -1,1 +1,24 @@
-public class 
+ import java.util.LinkedList;
+ 
+ public class HistorialNavegacion{
+     public static void main(String[] args) {
+
+        LinkedList<String> historial = new LinkedList<>();
+
+        historial.add("google.com");
+        historial.add("github.com");
+        historial.add("stackoverflow.com");
+
+        System.out.println("Página actual: " + historial.getLast());
+
+        historial.removeLast();
+
+        System.out.println("Después de ir atrás: " + historial.getLast());
+
+        System.out.println("Historial restante:");
+
+        for (String pagina : historial) {
+            System.out.println(pagina);
+        }
+    }
+}
